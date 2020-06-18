5d3f4d379ed99ddf12911cc99744b4e8befb1b10
jacksonrye <chijiokejackson35@gmail.com>
2020-06-18 15:01:29 +0100

    done, maybe make pwa later

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,9 +1,14 @@
+AOS.init({
+  duration: 1500,
+});
+
 // Select DOM Items
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 const navItems = document.querySelectorAll(".nav-item");
+const infos = document.querySelectorAll(".info");
 
 function toggleMenu() {
   menuBtn.classList.toggle("close");
@@ -12,5 +17,13 @@ function toggleMenu() {
   menuBranding.classList.toggle("show");
   navItems.forEach((item) => item.classList.toggle("show"));
 }
+
+function addFadeUp(ele) {
+  ele.setAttribute("data-aos", "fade-up");
+}
+
+navItems.forEach((item) => item.addEventListener("click", toggleMenu));
+
+// infos.forEach((info) => addFadeUp(info));
 
 menuBtn.addEventListener("click", toggleMenu);
